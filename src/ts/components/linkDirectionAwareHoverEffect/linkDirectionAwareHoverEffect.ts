32e9025de9ad5fcbf8eaa3d7ed44bfeb4dc17bc6
Lauris <edanriell@gmail.com>
2022-11-22 00:58:23 +0200

    fixed and refactored link direction aware hover effect

--- src/ts/components/linkDirectionAwareHoverEffect/linkDirectionAwareHoverEffect.ts
+++ src/ts/components/linkDirectionAwareHoverEffect/linkDirectionAwareHoverEffect.ts
@@ -1,26 +1,54 @@
-export class LinkDirectionAwareHoverEffect {
-	init() {
-		const links = document.querySelectorAll(".main-navigation__navigation-link");
+interface ILinkDirectionAwareHoverEffect {
+	links: NodeListOf<Element>;
+	color: string;
+	init(): void;
+}
 
-		links.forEach(element => {
-			element.addEventListener("mouseenter", event => {
-				// console.log("ENTER")
-				event.target?.addEventListener("mouseleave", event => {
-					event.stopImmediatePropagation();
-					// console.log("works leave2")
-					const tolerance = 10;
-					console.log(`tolerance = ${tolerance}`);
-					const left = 0;
-					console.log(`left = ${left}`);
-					const right = element.clientWidth;
-					console.log(`right = ${element.clientWidth}`);
-					let x = (event as MouseEvent).pageX - (element as HTMLAnchorElement).offsetLeft;
-					console.log(`x ${(event as MouseEvent).pageX} - ${(element as HTMLAnchorElement).offsetLeft} = ${x}`);
-					if (x - tolerance < left) x = left;
-					if (x + tolerance > right) x = right;
-					(element as HTMLAnchorElement).style.setProperty("--x", `${x}px`);
+export class LinkDirectionAwareHoverEffect implements ILinkDirectionAwareHoverEffect {
+	links: NodeListOf<Element>;
+	color: string;
+
+	constructor({
+		linksSelector,
+		underlineColor
+	}: {
+		linksSelector: string;
+		underlineColor?: string;
+	}) {
+		this.links = document.querySelectorAll(linksSelector);
+		this.color = underlineColor || "blue";
+	}
+
+	public init(): void {
+		this.links.forEach(element => {
+			this.setUnderlineColor(this.color, element);
+			["mouseenter", "mouseleave"].forEach(eventType => {
+				element.addEventListener(eventType, event => {
+					this.#showUnderline(element, event);
 				});
 			});
 		});
+	}
+
+	private setUnderlineColor(color: string, element: Element): void {
+		(element as HTMLAnchorElement).style.setProperty("--underlineColor", `${color}`);
+	}
+
+	#showUnderline(element: Element, event: Event): void {
+		this.#calculateUnderlineStartingPoint(element, event);
+	}
+
+	#calculateUnderlineStartingPoint(element: Element, event: Event): void {
+		const tolerance = 10;
+		const left = 0;
+		const right = element.clientWidth;
+		let x = (event as MouseEvent).pageX - (element as HTMLAnchorElement).offsetLeft;
+		if (x - tolerance < left) x = left;
+		if (x + tolerance > right) x = right;
+		this.#displayUnderline(element, x);
+	}
+
+	#displayUnderline(element: Element, x: number): void {
+		(element as HTMLAnchorElement).style.setProperty("--x", `${x}px`);
 	}
 }
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -3,6 +3,10 @@ import "../scss/style.scss";
 import { LinkDirectionAwareHoverEffect } from "./components/linkDirectionAwareHoverEffect";
 
 window.addEventListener("DOMContentLoaded", () => {
-	const linkDirectionAwareHoverEffect = new LinkDirectionAwareHoverEffect();
+	const linkDirectionAwareHoverEffect = new LinkDirectionAwareHoverEffect({
+        linksSelector: ".main-navigation__navigation-link",
+        underlineColor: "hsl(177deg, 100%, 39%)"
+    });
+
 	linkDirectionAwareHoverEffect.init();
 });
