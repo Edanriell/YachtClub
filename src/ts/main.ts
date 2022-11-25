f85ff13bce8d2005263b10f42d4854c4b239746d
Lauris <edanriell@gmail.com>
2022-11-26 00:17:44 +0200

    working on checkbox

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -18,44 +18,16 @@ window.addEventListener("DOMContentLoaded", () => {
 
 	mainNavigationLinkDirectionAwareHoverEffect.init();
 
+	    
+	let checkbox = document.getElementById('cb');
+	let svg = document.getElementById('checkbox');
 
-	const animateTickMark = gsap.timeline({ paused: true });
-const animateCheckBox = gsap.timeline({ paused: true, yoyo: true });
-
-let toggle = true;
-
-animateTickMark
-  .set(".checkbox", { border: "2px solid #3F71FF" })
-  .to(".fill-color", 0.1, {
-    webkitClipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)"
-  })
-  .to(".tick-mark-1", 0.1, { webkitClipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)" })
-  .to(".tick-mark-2", 0.1, { webkitClipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)" });
-
-animateCheckBox
-  .to(".checkbox", 0.1, { rotate: 10 }, 0.2)
-  .to(".checkbox", 0.1, { rotate: 0 }, 0.3)
-  .to(".line-1", 0.1, { width: 15 }, 0.3)
-  .to(".line-2", 0.1, { width: 15 }, 0.3)
-  .to(".line-3", 0.1, { width: 15 }, 0.3)
-  .to(".line-4", 0.1, { width: 15 }, 0.3)
-  .set(".line-1", { width: 0 })
-  .set(".line-2", { width: 0 })
-  .set(".line-3", { width: 0 })
-  .set(".line-4", { width: 0 })
-
-  const checkbox = document.querySelector(".checkbox");
-
-  checkbox?.addEventListener("click", () => {
-	console.log(checkbox);
-	if(toggle){
-		animateTickMark.restart();
-		animateCheckBox.restart();
-	   } else {
-		animateTickMark.reverse();
-	   }
-	   toggle = !toggle;
-  })
+	svg?.addEventListener('click', () => {
+		if((checkbox as HTMLInputElement).checked)
+			svg?.classList.add('reverse')
+		else 
+			svg?.classList.remove('reverse')
+	})
 
 });
 
