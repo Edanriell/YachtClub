5a4569bb9c66855e5401acfb8a9f70933c3da3ac
Lauris <edanriell@gmail.com>
2022-11-25 07:40:11 +0200

    started work on checkbox

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,6 +1,8 @@
 import "../scss/style.scss";
 import "swiper/css";
 
+import gsap from "gsap";
+
 import Swiper, { Navigation, Autoplay, Keyboard, EffectCreative } from "swiper";
 import { HeroSlider } from "./components/HeroSlider";
 import { LinkDirectionAwareHoverEffect } from "./components/LinkDirectionAwareHoverEffect";
@@ -15,6 +17,46 @@ window.addEventListener("DOMContentLoaded", () => {
 	});
 
 	mainNavigationLinkDirectionAwareHoverEffect.init();
+
+
+	const animateTickMark = gsap.timeline({ paused: true });
+const animateCheckBox = gsap.timeline({ paused: true, yoyo: true });
+
+let toggle = true;
+
+animateTickMark
+  .set(".checkbox", { border: "2px solid #3F71FF" })
+  .to(".fill-color", 0.1, {
+    webkitClipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)"
+  })
+  .to(".tick-mark-1", 0.1, { webkitClipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)" })
+  .to(".tick-mark-2", 0.1, { webkitClipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)" });
+
+animateCheckBox
+  .to(".checkbox", 0.1, { rotate: 10 }, 0.2)
+  .to(".checkbox", 0.1, { rotate: 0 }, 0.3)
+  .to(".line-1", 0.1, { width: 15 }, 0.3)
+  .to(".line-2", 0.1, { width: 15 }, 0.3)
+  .to(".line-3", 0.1, { width: 15 }, 0.3)
+  .to(".line-4", 0.1, { width: 15 }, 0.3)
+  .set(".line-1", { width: 0 })
+  .set(".line-2", { width: 0 })
+  .set(".line-3", { width: 0 })
+  .set(".line-4", { width: 0 })
+
+  const checkbox = document.querySelector(".checkbox");
+
+  checkbox?.addEventListener("click", () => {
+	console.log(checkbox);
+	if(toggle){
+		animateTickMark.restart();
+		animateCheckBox.restart();
+	   } else {
+		animateTickMark.reverse();
+	   }
+	   toggle = !toggle;
+  })
+
 });
 
 window.addEventListener("load", () => {
