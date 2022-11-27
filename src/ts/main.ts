c5f288ea6fa3b0fb073ff23297ad49ece445b49c
Lauris <edanriell@gmail.com>
2022-11-27 22:11:36 +0200

    started work on form

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -6,6 +6,8 @@ import { HeroSlider } from "./components/HeroSlider";
 import { LinkDirectionAwareHoverEffect } from "./components/LinkDirectionAwareHoverEffect";
 import { VideoPlayer } from "./components/VideoPlayer";
 import { CustomCheckbox } from "./components/CustomCheckbox";
+import { Forms } from "./components/Forms";
+import { FormValidation } from "./components/FormValidation";
 
 Swiper.use([Navigation, Autoplay, Keyboard, EffectCreative]);
 
@@ -21,8 +23,107 @@ window.addEventListener("DOMContentLoaded", () => {
 		reverseAnimationClassName: "custom-checkbox__reverse-animation"
 	});
 
+	const eventParticipantRegistrationForm = new Forms({
+		formSelector: "[data-event-participant-registration-form]",
+		submitButtonSelector: "[data-event-participant-registration-form-submit-button]",
+		databaseName: "eventParticipants"
+	});
+
+	const eventParticipantRegistrationFormValidation = new FormValidation({
+		form: "[data-event-participant-registration-form]",
+		inputs: [
+			{
+				uniqueName: "name",
+				selector: "[data-event-participant-registration-form-name-input]",
+				testRegExp:
+					// eslint-disable-next-line max-len
+					/^[a-zа-яё\s]+$/iu,
+				style: {
+					valid: `
+						border: 0.1rem solid darkgreen;
+					`,
+					unvalid: `
+						border: 0.1rem solid darkred;
+					`
+				},
+				errorMessage: {
+					messageText: "Неверное имя",
+					messageStyle: `
+						color: darkred;
+						position: absolute;
+						font-size: 1.1rem;
+						bottom: 0;
+						right: 0;
+						z-index: 4;
+						font-family: var(--ff-normal-400);
+					`
+				}
+			},
+			{
+				uniqueName: "phoneNumber",
+				selector: "[data-event-participant-registration-form-phone-input]",
+				testRegExp:
+					// eslint-disable-next-line max-len
+					/^(\+)\d+$/,
+				style: {
+					valid: `
+						border: 0.1rem solid darkgreen;
+					`,
+					unvalid: `
+						border: 0.1rem solid darkred;
+					`
+				},
+				errorMessage: {
+					messageText: "Неверный номер телефона",
+					messageStyle: `
+						color: darkred;
+						position: absolute;
+						font-size: 1.1rem;
+						bottom: 0;
+						right: 0;
+						z-index: 4;
+						font-family: var(--ff-normal-400);
+					`
+				}
+			},
+			{
+				uniqueName: "email",
+				selector: "[data-event-participant-registration-form-email-input]",
+				testRegExp:
+					// eslint-disable-next-line max-len
+					/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g,
+				style: {
+					valid: `
+						border: 0.1rem solid darkgreen;
+					`,
+					unvalid: `
+						border: 0.1rem solid darkred;
+					`
+				},
+				errorMessage: {
+					messageText: "Неверный адрес электронной почты",
+					messageStyle: `
+						color: darkred;
+						position: absolute;
+						font-size: 1.1rem;
+						bottom: 1rem;
+						right: 0;
+						z-index: 4;
+						font-family: var(--ff-normal-400);
+					`
+				}
+			}
+		],
+		initialInputStyle: `
+			border: 0.1rem solid var(--clr-primary-nileBlueHalfTransparent);
+		`,
+		submitButton: "[data-event-participant-registration-form-submit-button]"
+	});
+
 	mainNavigationLinkDirectionAwareHoverEffect.init();
 	customCheckbox.init();
+	eventParticipantRegistrationForm.init();
+	eventParticipantRegistrationFormValidation.init();
 });
 
 window.addEventListener("load", () => {
